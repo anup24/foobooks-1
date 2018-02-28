7b01acf8ddad1acf9c1ae2695cbe27324211962f
Susan Buck <susan@susanbuck.net>
2018-02-28 17:47:21 -0500

    Remove temp code I had added to take a screenshot for the notes

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -6,14 +6,11 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    public function index()
-    {
-        return App::environment(); # <- This is what we're testing out
-        #return 'Here are all the books...';
+    public function index() {
+        return 'Here are all the books in the library';
     }
 
-    public function show($title)
-    {
+    public function show($title) {
         return 'You are viewing the book ' . $title;
     }
 }
